38ec878d4c28bdfc538b37e1ec461a3be260373d
kamey <tesla.vm123@gmail.com>
2021-03-29 10:47:31 +0200

    The code has been cleared and some of the functions have been rewritten, added the language section to the config.yml file so that users can change all messages from plugin and prefixes, better version system, merged contributions from syscraft and Arctic, better checking for if the user is console or a real player, some changes to prevent crashing and improve performance by saving ram.

--- src/main/java/me/kamey/Main.java
+++ src/main/java/me/kamey/Main.java
@@ -1,159 +1,96 @@
 package me.kamey;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-
 import java.util.Objects;
-import java.util.logging.Logger;
 
 public final class Main extends JavaPlugin implements Listener {
 
-    public Logger log = Bukkit.getLogger();
-
-    String plugin_name = ChatColor.translateAlternateColorCodes('&',"&2[KSpawn] ");
-
+    String pluginName = ChatColor.translateAlternateColorCodes('&', getConfig().getString("language.prefix") + " ");
     private Location location;
-
 
     public void onEnable() {
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
-        loadLoc();
         loadConfig();
-        log.info(plugin_name + ChatColor.translateAlternateColorCodes('&',"&bIs Starting!"));
-        log.info(plugin_name + ChatColor.translateAlternateColorCodes('&',"&bMade by Kamey"));
-        log.warning(plugin_name + ChatColor.translateAlternateColorCodes('&',"&6This plugin is intended to be used with java 11 and newer"));
+        loadLoc();
+        getLogger().info(ChatColor.translateAlternateColorCodes('&', "&bStarting!"));
+        getLogger().info(ChatColor.translateAlternateColorCodes('&', "&bThis plugin is made by Kamey"));
     }
-
 
     public void onDisable() {
-        log.info(plugin_name + ChatColor.translateAlternateColorCodes('&',"&bStopping!"));
     }
-
 
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         Player p = (Player) sender;
         if (cmd.getName().equalsIgnoreCase("setjoinspawn")) {
-            if (sender != null) {
+            //this is to check if its not console?
+            if (!(sender instanceof ConsoleCommandSender)) {
                 if (p.hasPermission("spawnonjoin.admin")) {
                     if (args.length == 0) {
                         this.location = p.getLocation();
                         saveLoc(this.location);
-
-                        p.sendMessage(String.valueOf(this.plugin_name) + ChatColor.translateAlternateColorCodes('&',"&aSpawn set!"));
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&',"   &7World: " + getConfig().getString("location.world")));
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&',"   &7x: " + getConfig().getDouble("location.x")));
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&',"   &7y: " + getConfig().getDouble("location.y")));
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&',"   &7z: " + getConfig().getDouble("location.z")));
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&',"   &7yaw: " + getConfig().getString("location.yaw")));
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&',"   &7pitch: " + getConfig().getString("location.pitch")));
+                        p.sendMessage(this.pluginName + ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.spawn-set"))));
                         return true;
-
                     }
-
-                    p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&cThere was an error, please do: /setjoinspawn"));
-
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.error"))));
                     return true;
-
                 }
-
-                p.sendMessage(String.valueOf(this.plugin_name) + ChatColor.translateAlternateColorCodes('&',"&cNo Permission"));
-
+                p.sendMessage(this.pluginName + ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.no-permission"))));
                 return true;
-
+            } else {
+                sender.sendMessage(this.pluginName + ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.not-a-player"))));
             }
-
-            assert sender != null;
-            sender.sendMessage("Only Players can run this commands!");
-
             return true;
-
-        }
-
-        if (cmd.getName().equalsIgnoreCase("spawn")) {
-
-
-            if (sender != null) {
-
-
+        } else if (cmd.getName().equalsIgnoreCase("spawn")) {
+            //make sure sender is not console
+            if (!(sender instanceof ConsoleCommandSender)) {
                 if (p.hasPermission("spawnonjoin.command")) {
-
-
                     if (args.length == 0) {
-
-
                         if (getConfig().getString("location.world") != null) {
-
-
-                            p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&bTeleporting..."));
+                            p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.teleporting"))));
                             p.teleport(this.location);
-
                             return true;
-
                         }
-
-                        p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&7The spawn point is not set, to set spawn do: /setjoinspawn"));
-
+                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.spawn-not-set"))));
                         return true;
-
                     }
-
-                    p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&7The spawn point is not set, to set spawn do: /setjoinspawn"));
-
+                    p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.spawn-not-set"))));
                     return true;
-
                 }
-
-                p.sendMessage(String.valueOf(this.plugin_name) + ChatColor.translateAlternateColorCodes('&',"&cNo Permission!"));
-
+                p.sendMessage(this.pluginName + ChatColor.translateAlternateColorCodes('&', "&cNo Permission!"));
                 return true;
-
+            } else {
+                sender.sendMessage(this.pluginName + ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.not-a-player"))));
             }
-
-            assert sender != null;
-            sender.sendMessage("Only Players can run this commands!");
-
             return true;
-
         }
-
         return true;
-
     }
 
-
     public void loadLoc() {
-
         if (getConfig().getString("location.world") != null) {
-
             this.location = new Location(getServer().getWorld(Objects.requireNonNull(getConfig().getString("location.world"))),
                     getConfig().getDouble("location.x"),
                     getConfig().getDouble("location.y"),
                     getConfig().getDouble("location.z"),
                     Float.parseFloat(Objects.requireNonNull(getConfig().getString("location.yaw"))),
                     Float.parseFloat(Objects.requireNonNull(getConfig().getString("location.pitch"))));
-
         }
-
     }
-
 
     public void loadConfig() {
-
         getConfig().options().copyDefaults(true);
-
-        saveConfig();
-
+        saveDefaultConfig();
     }
-
 
     public void saveLoc(Location location) {
         getConfig().set("location", location);
@@ -167,37 +104,23 @@ public final class Main extends JavaPlugin implements Listener {
 
     }
 
-
     @EventHandler
     public void onRespawn(PlayerRespawnEvent e) {
-
         Player p = e.getPlayer();
-
         if (getConfig().getString("location.world") != null) {
-
             e.setRespawnLocation(this.location);
-
         } else {
-
-            p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&7The spawn point is not set, to set spawn do: /setjoinspawn"));
-
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.spawn-not-set"))));
         }
-
     }
-
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-
         Player p = e.getPlayer();
-
         if (getConfig().getString("location.world") != null) {
-
             p.teleport(this.location);
-
         } else {
-
-            p.sendMessage(ChatColor.translateAlternateColorCodes('&',"&7The spawn point is not set, to set spawn do: /setjoinspawn"));
+            p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("language.spawn-not-set"))));
         }
 
     }
